0ccc69b1f90a7c202133004e921958ca2f06a58a
guilhermerecife <guilherme@hotmail.com>
2015-08-01 19:46:56 -0300

    inicio

---
+++ ntalk/app.js
@@ -0,0 +1,22 @@
+var express = require('express');
+var load = require('express-load');
+var bodyParser = require('body-parser');
+var cookieParser = require('cookie-parser');
+var expressSession = require('express-session');
+var methodOverride = require('method-override');
+var app = express();
+
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(cookieParser('ntalk'));
+app.use(expressSession());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(methodOverride('_method'));
+app.use(express.static(__dirname + '/public'));
+
+load('models').then('controllers').then('routes').into(app);
+
+app.listen(80, function() {
+	console.log('ntalk no ar!');
+});
