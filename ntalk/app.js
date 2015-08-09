0753b600cafcb60099926fbe56643bb518365b62
guilhermerecife <guilherme@hotmail.com>
2015-08-09 00:47:05 -0300

    Melhoramento do chat

--- ntalk/app.js
+++ ntalk/app.js
@@ -1,3 +1,4 @@
+const KEY = 'ntalk.sid', SECRET = 'ntalk';
 var express = require('express');
 var load = require('express-load');
 var bodyParser = require('body-parser');
@@ -8,30 +9,42 @@ var error = require('./middlewares/error');
 var app = express();
 var server = require('http').Server(app);
 var io = require('socket.io')(server);
+var cookie = cookieParser(SECRET);
+var store = new expressSession.MemoryStore();
 
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
-app.use(cookieParser('ntalk'));
+app.use(cookie);
 app.use(expressSession({
-	saveUninitialized: false,
-	resave: false,
-	secret: 'ntalk'
+	secret: SECRET,
+	name: KEY,
+	resave: true,
+	saveUninitialized: true,
+	store: store
 }));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(methodOverride('_method'));
 app.use(express.static(__dirname + '/public'));
 
-load('models').then('controllers').then('routes').into(app);
-
-io.sockets.on('connection', function(client) {
-	client.on('send-server', function(data) {
-		console.log("Enviando mensagem no chat!");
-		var msg = "<b>" + data.nome + ":</b> " + data.msg + "<br>";
-		client.emit('send-client', msg);
-		client.broadcast.emit('send-client', msg);
+io.use(function(socket, next) {
+	var data = socket.request;
+	cookie(data, {}, function(err) {
+		var sessionID = data.signedCookies[KEY];
+		store.get(sessionID, function(err, session) {
+			if(err || !session) {
+				return next(new Error('Acesso negado'));
+			}else {
+				socket.handshake.session = session;
+				return next();
+			}
+		});
 	});
 });
+
+load('models').then('controllers').then('routes').into(app);
+
+load('sockets').into(io);
 
 app.use(error.notFound);
 app.use(error.serverError);
--- ntalk/controllers/chat.js
+++ ntalk/controllers/chat.js
@@ -2,8 +2,7 @@
 module.exports = function() {
 	var ChatController = {
 		index: function(req, res) {
-			var params = {usuario: req.session.usuario};
-			res.render('chat/index', params);
+			res.render('chat/index');
 		}
 	};
 	return ChatController;
