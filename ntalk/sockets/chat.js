0969e64287b063c69ce42226490e14a7ef70bb0f
guilhermerecife <guilherme@hotmail.com>
2015-09-05 11:01:29 -0300

    TESTE

--- ntalk/sockets/chat.js
+++ ntalk/sockets/chat.js
@@ -1,10 +1,16 @@
 //Sockets
 module.exports = function(io) {
 	var crypto = require('crypto')
-	, sockets = io.sockets;
+	, sockets = io.sockets
+	, onlines = {};
 	sockets.on('connection', function(client) {
 		var session = client.handshake.session;
 		var usuario = session.usuario;
+		onlines[usuario.email] = usuario.email;
+		for (var email in onlines) {
+			client.emit('notify-onlines', email);
+			client.broadcast.emit('notify-onlines', email);
+		}
 		client.on('send-server', function(msg) {
 			var sala = session.sala,
 			data = {email: usuario.email, sala: sala};
@@ -22,6 +28,11 @@ module.exports = function(io) {
 		client.join(sala);
 		});
 		client.on('disconnect', function () {
+			var sala = session.sala
+			, msg = "<b>"+ usuario.nome +":</b> saiu.<br>";
+			client.broadcast.emit('notify-offlines', usuario.email);
+			sockets.in(sala).emit('send-client', msg);
+			delete onlines[usuario.email];
 			client.leave(session.sala);
 		});
 	});
